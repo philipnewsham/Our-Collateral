79fc59e3a0369de9b052d9b3404a84b07508e04d
PhilipN <philip.newsham@hotmail.co.uk>
2018-06-15 10:46:38 +0100

    cleaned alien colour change script

--- Assets/Scripts/Enemy AI/AlienColourChange.cs
+++ Assets/Scripts/Enemy AI/AlienColourChange.cs
@@ -1,35 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AlienColourChange : MonoBehaviour {
-	public bool alarmed = false;
+public class AlienColourChange : MonoBehaviour
+{
+	private bool alarmed = false;
 	private int randNo;
 	private ParticleSystem ptcl;
-	// Use this for initialization
-	void Start () {
+
+	void Start ()
+    {
 		ptcl = GetComponent<ParticleSystem> ();
 	}
+
+    public void SetAlarmState(bool alarmState)
+    {
+        alarmed = alarmState;
+    }
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		randNo = Random.Range (1,3);
-		if (alarmed == true) {
-			//Alarm();
-			if(randNo == 1){
-				ptcl.startColor = new Color (1, 0, 0, 1);
-			}else{
-				ptcl.startColor = new Color (1, 1, 0, 1);
-			}
-		} else {
-			if(randNo == 1){
-				ptcl.startColor = new Color (0, 0, 1, 1);
-			}else{
-				ptcl.startColor = new Color (0, 1, 1, 1);
-			}
-		}
+        //if alarmed random between red (1,0,0,1) or yellow (1,1,0,1)
+        //if not alarmed random between cyan (0,1,1,1) or blue (0,0,1,1)
+		ptcl.startColor = new Color ((alarmed ? 1 : 0), (randNo == 1 ? 0 : 1), (alarmed ? 0 : 1), 1);
 	}
-//	void Alarm(){
-//		ptcl.startColor[0] = new Color (1, 0, 0, 1);
-//		ptcl.startColor[1] = new Color (1, 1, 0, 1);
-//	}
 }
