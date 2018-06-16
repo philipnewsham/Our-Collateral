60d37bc3ce57461114dfe3649430c1bfe7c24fdb
PhilipN <philip.newsham@hotmail.co.uk>
2018-06-16 23:08:05 +0100

    cleaned button script

--- Assets/Scripts/Vault/Colour.cs
+++ Assets/Scripts/Vault/Colour.cs
@@ -6,7 +6,6 @@ public class Colour : MonoBehaviour
 	public float red;
 	public float green;
 	public float blue;
-    
 	public float redLight;
 	private float remainderRed;
 	public float greenLight;
@@ -16,6 +15,7 @@ public class Colour : MonoBehaviour
 	private float redHint;
 	private float colourDiff = 0.02f;
 	public GameObject[] floors;
+    private Renderer[] floorRenderers;
 	private int hintCount = 4;
 	private int hintRand;
 
@@ -31,15 +31,20 @@ public class Colour : MonoBehaviour
 
 	void Start ()
     {
-		buttons = GameObject.FindGameObjectsWithTag ("Button");
-		floors = GameObject.FindGameObjectsWithTag ("Floor");
-		vault = GameObject.FindGameObjectWithTag ("VaultMain");
+		buttons = GameObject.FindGameObjectsWithTag("Button");
+		floors = GameObject.FindGameObjectsWithTag("Floor");
+        floorRenderers = new Renderer[floors.Length];
+
+        for (int i = 0; i < floors.Length; i++)
+            floorRenderers[i] = floors[i].GetComponent<Renderer>();
+
+		vault = GameObject.FindGameObjectWithTag("VaultMain");
 		vaultCamera.GetComponent<Camera>().enabled = false;
-		mainCamera = GameObject.FindGameObjectWithTag ("MainCamera");
-		player = GameObject.FindGameObjectWithTag ("Player");
+		mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+		player = GameObject.FindGameObjectWithTag("Player");
+
 		blueLight = Random.Range (0.05f, 1f);
 		remainderBlue = ((blueLight * 100) % 5);
-
 		if (remainderBlue > 0)
 			blueLight = (((blueLight * 100)-remainderBlue)/100);
 
@@ -53,133 +58,101 @@ public class Colour : MonoBehaviour
 		if (remainderGreen > 0) 
 			greenLight = (((greenLight * 100) - remainderGreen) / 100);
 
-		vault.GetComponent<Renderer> ().material.color = new Color (redLight, greenLight, blueLight, 0);
+		vault.GetComponent<Renderer>().material.color = new Color(redLight, greenLight, blueLight, 0);
 	}
 
-	void FixedUpdate ()
+    void UpdateColours()
     {
-		for (int i = 0; i < floors.Length; i ++)
-			floors [i].GetComponent<Renderer> ().material.color = new Color (red, green, blue, 0);
+        foreach (Renderer renderer in floorRenderers)
+            renderer.material.color = new Color(red, green, blue, 0);
 
-		if (red > 1f) 
-			red = 1f;
-
-        if (green > 1f)
-            green = 1f;
-		
-		if (blue > 1f) 
-			blue = 1f;
-		
-			if ((red >= redLight - colourDiff) && (red <= redLight + colourDiff))
+        if ((red >= redLight - colourDiff) && (red <= redLight + colourDiff))
         {
-			if ((green >= greenLight - colourDiff) && (green <= greenLight + colourDiff))
+            if ((green >= greenLight - colourDiff) && (green <= greenLight + colourDiff))
             {
-				if ((blue >= blueLight - colourDiff) && (blue <= blueLight + colourDiff))
+                if ((blue >= blueLight - colourDiff) && (blue <= blueLight + colourDiff))
                 {
-					if(camCount > 0)
+                    if (camCount > 0)
                     {
-						camCount -= 1;
-						VaultOpen ();
-						vaultOpen = true;
-					}
+                        camCount -= 1;
+                        VaultOpen();
+                        vaultOpen = true;
+                    }
                     else
                     {
-						player.SetActive(true);
-						mainCamera.GetComponent<Camera>().enabled = true;
-						mainCamera.GetComponent<AudioListener> ().enabled = true;
-						vaultCamera.GetComponent<AudioListener> ().enabled = false;
-						vaultCamera.GetComponent<Camera>().enabled = false;
-					}
-				}	
-			}
-		}
-	}
+                        player.SetActive(true);
+                        mainCamera.GetComponent<Camera>().enabled = true;
+                        mainCamera.GetComponent<AudioListener>().enabled = true;
+                        vaultCamera.GetComponent<AudioListener>().enabled = false;
+                        vaultCamera.GetComponent<Camera>().enabled = false;
+                    }
+                }
+            }
+        }
+    }
 
-	public void Blue1()
+    public void SetColourValue(ButtonInfo.Colour colour, ButtonInfo.Size size)
     {
-		blue += 0.05f;
-	}
+        float[] sizes = new float[3] { 0.05f, 0.1f, 0.2f };
+        switch (colour)
+        {
+            case ButtonInfo.Colour.RED:
+                red = Mathf.Clamp(red + sizes[(int)size], 0.0f, 1.0f);
+                break;
+            case ButtonInfo.Colour.GREEN:
+                green = Mathf.Clamp(green + sizes[(int)size], 0.0f, 1.0f);
+                break;
+            case ButtonInfo.Colour.BLUE:
+                blue = Mathf.Clamp(blue + sizes[(int)size], 0.0f, 1.0f);
+                break;
+        }
+        Debug.Log("set colour value");
+        UpdateColours();
+    }
 
-	public void Blue2()
+    public void ResetColourValue(ButtonInfo.Colour colour)
     {
-		blue += 0.1f;
-	}
-
-	public void Blue4()
-    {
-		blue += 0.2f;
-	}
-
-	public void BlueReset()
-    {
-		blue = 0f;
-	}
-
-	public void Red1()
-    {
-		red += 0.05f;
-	}
-
-	public void Red2()
-    {
-		red += 0.1f;
-	}
-
-	public void Red4()
-    {
-		red += 0.2f;
-	}
-
-	public void RedReset()
-    {
-		red = 0f;
-	}
-
-	public void Green1()
-    {
-		green += 0.05f;
-	}
-
-	public void Green2()
-    {
-		green += 0.1f;
-	}
-
-	public void Green4()
-    {
-		green += 0.2f;
-	}
-
-	public void GreenReset()
-    {
-		green = 0f;
-	}
-
+        switch (colour)
+        {
+            case ButtonInfo.Colour.RED:
+                red = 0.0f;
+                break;
+            case ButtonInfo.Colour.GREEN:
+                green = 0.0f;
+                break;
+            case ButtonInfo.Colour.BLUE:
+                blue = 0.0f;
+                break;
+        }
+        UpdateColours();
+    }
+    
 	public void Reset()
     {
 		red = 0f;
 		green = 0f;
 		blue = 0f;
 		hintCount = 4;
+        UpdateColours();
 	}
 
 	public void Hint()
     {
-		if ((red > 0f) && (blue > 0f) && (green > 0))
+		if (red > 0.0f && blue > 0.0f && green > 0.0f)
         {
-			if (hintCount > -1)
+			if (hintCount >= 0)
             {
 				hintCount -= 1;
 				hintRand = Random.Range (1, 4);
 				if (hintRand == 1)
-					print (((red - redLight) + ": " + hintCount + " hints remaining."));
+					Debug.LogFormat("{0}: {1} hints remaining.", red - redLight, hintCount);
 				else if (hintRand == 2)
-					print (((green - greenLight) + ": " + hintCount + " hints remaining."));
+                    Debug.LogFormat("{0}: {1} hints remaining.", green - greenLight, hintCount);
 				else
-					print (((blue - blueLight) + ": " + hintCount + " hints remaining."));
-			}else
+                    Debug.LogFormat("{0}: {1} hints remaining.", blue - blueLight, hintCount);
+            }
+            else
 				print("No hints remaining, hit reset to try again!");
-			
 		}
         else
 			print ("What are you trying to do, cheat?");
@@ -189,13 +162,13 @@ public class Colour : MonoBehaviour
     {
 		GetComponent<BoxCollider> ().isTrigger = true;
 		mainCamera.GetComponent<Camera>().enabled = false;
-		mainCamera.GetComponent<AudioListener> ().enabled = false;
-		vaultCamera.GetComponent<AudioListener> ().enabled = true;
+		mainCamera.GetComponent<AudioListener>().enabled = false;
+		vaultCamera.GetComponent<AudioListener>().enabled = true;
 		vaultCamera.GetComponent<Camera>().enabled = true;
 
 		player.SetActive(false);
-		for (int x = 0; x < buttons.Length; x++)
-			Destroy(buttons[x]);
+		foreach(GameObject button in buttons)
+			Destroy(button);
 
 		transform.position = new Vector3 (transform.position.x, transform.position.y - 0.03f, transform.position.z);
 	}
---
+++ Assets/Scripts/Vault/ColourButton.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ButtonInfo
+{
+    public enum Size
+    {
+        SMALL,
+        MEDIUM,
+        LARGE
+    }
+
+    public enum Colour
+    {
+        RED,
+        GREEN,
+        BLUE
+    }
+}
+
+public class ColourButton : MonoBehaviour
+{
+	private GameObject vault;
+    private Colour colourScript;
+	private bool enter = false;
+	public bool buttonReset;
+	public bool vaultOpen;
+	AudioSource audioSource;
+    public ButtonInfo.Size buttonSize;
+    public ButtonInfo.Colour buttonColour;
+
+    void Start()
+    {
+		vault = GameObject.FindGameObjectWithTag("Vault");
+		audioSource = GetComponent<AudioSource>();
+        colourScript = vault.GetComponent<Colour>();
+        StartCoroutine(WaitForUnlock());
+	}
+
+	void Update()
+    {
+		if (Input.GetKeyUp (KeyCode.E))
+			enter = false;
+	}
+
+    IEnumerator WaitForUnlock()
+    {
+        yield return new WaitUntil(() => colourScript.vaultOpen);
+        GetComponent<MeshRenderer>().enabled = false;
+        this.enabled = false;
+    }
+
+	void OnTriggerStay(Collider other)
+    {
+		if ((other.gameObject.tag == "Player"))
+        {
+			if(Input.GetKeyDown(KeyCode.E) && !enter)
+            {
+				enter = true;
+                if (buttonReset)
+                    colourScript.ResetColourValue(buttonColour);
+                else
+                    colourScript.SetColourValue(buttonColour, buttonSize);
+                audioSource.Play();
+            }
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+    {
+		if (other.gameObject.tag == "Player")
+			enter = false;
+	}
+}
